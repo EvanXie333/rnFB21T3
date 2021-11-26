2179d941ed66b7a1daac6dfe93b471e6da947131
evanxie <1007979078@q.com>
2021-11-27 10:52:08 +1100

    rename the function name

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View,Text, StyleSheet } from "react-native";
 
-export function Signin(props){
+export function Home (props){
     return(
         <View>
             <Text>Home</Text>
--- components/Signup.js
+++ components/Signup.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View,Text, StyleSheet } from "react-native";
+import { View,Text, StyleSheet, Button } from "react-native";
 
-export function Signin(props){
+export function Signup(props){
     return(
         <View>
             <Text>Sign up</Text>
+            <Button title="Click here to sign in"/>
         </View>
     )
 }
