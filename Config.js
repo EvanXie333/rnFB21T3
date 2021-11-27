1d055f21c4b6aaaccc4047e6ce909a789714a25b
evanxie <1007979078@q.com>
2021-11-27 11:40:47 +1100

    adding firebase

---
+++ Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAgHLn1ZVFtZzDPHQByYMc7Mfff5q4IOhw",
+    authDomain: "rnfirebase-50e6c.firebaseapp.com",
+    projectId: "rnfirebase-50e6c",
+    storageBucket: "rnfirebase-50e6c.appspot.com",
+    messagingSenderId: "260626591851",
+    appId: "1:260626591851:web:49b53ddca5d19c232c412e",
+    measurementId: "G-YSK2V8QLH0"
+  };
--- components/Signup.js
+++ components/Signup.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { View,Text, StyleSheet, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+
 
 export function Signup(props){
+
+    const navigation = useNavigation()
+
     return(
         <View>
             <Text>Sign up</Text>
-            <Button title="Click here to sign in"/>
+            <Button title="Click here to sign in" onPress ={ ()=> navigation.navigate("Signin")} />
         </View>
     )
 }
