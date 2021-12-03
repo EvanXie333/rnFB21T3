02a43147bb86de64b49f0d8321876ede5822a676
evanxie <1007979078@q.com>
2021-12-03 15:40:24 +1100

    addding signout

--- components/Signout.js
+++ components/Signout.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import {StyleSheet, TouchableOpacity, Text} from 'react-native'
+
+
 export function Signout( props){
+
+    const handlePrss =() =>{
+
+    }
     return(
-        <TouchableOpacity>
+        <TouchableOpacity onPress= { () => props.handler()}>
             <Text style={styles.signoutText}>Sign out</Text>
         </TouchableOpacity>
     )
